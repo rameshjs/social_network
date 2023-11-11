a3177cc775e88de77c89a6300524ff8ce72ed3c6
Ramesh Kumar <rameshkumar.exe@gmail.com>
2023-11-11 08:12:11 +0530

    feat: Custom user model and admin config update

--- social_network/settings.py
+++ social_network/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -80,6 +81,8 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+AUTH_USER_MODEL = "users.User"
 
 
 # Password validation
